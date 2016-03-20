b261abc631710fa9720ae815a9fd9c9f4fb324a5
Rebecca Estes <estes.rebecca@gmail.com>
2016-03-19 22:28:38 -0400

    initial commit

---
+++ db/schema.js
@@ -0,0 +1,18 @@
+// requiring mongoose dependency
+var mongoose = require('mongoose');
+// var conn = mongoose.connect(process.env.PROD_MONGODB || 'mongodb://127.0.0.1:27017/locations');
+
+// instantiate a name space for our Schema constructor defined by mongoose.
+var Schema = mongoose.Schema,
+    ObjectId = Schema.ObjectId;
+
+// defining schema for locations.
+var MessageSchema = new Schema({
+  name: String,
+  phone: String,
+  email: String
+});
+
+// setting models in mongoose utilizing schemas defined above, we'll be using
+// these frequently throughout our app
+mongoose.model("Message", MessageSchema);
---
+++ script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+
+	$('button').on('click', function() {
+		$('.form').hide();
+		$('.thanks').show();
+	})
+
+})
