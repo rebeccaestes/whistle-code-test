339ad2fe37c236499fd0d4e3f9ae75b662cf1c7b
Rebecca Estes <estes.rebecca@gmail.com>
2016-03-20 22:28:14 -0400

    added correct port

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(express.static(__dirname + '/public'))
 
-app.listen(4000, function(){
+app.listen(process.env.PORT || 4000, function(){
   console.log("app listening on port 4000")
 })
 
