5f8e20145cd91e668f9a95ac253ec0242de171fb
Rebecca Estes <estes.rebecca@gmail.com>
2016-03-20 21:51:34 -0400

    cleaning up code

--- controllers/messagesController.js
+++ controllers/messagesController.js
@@ -2,26 +2,20 @@ var MessageModel = require("../models/message")
 
 var messagesController = {
   index: function(req, res){
-    MessageModel.find({}, function(err){
       res.render("messages/index");
-    })
+  },
+  thanks: function(req, res){
+      res.render("messages/thanks");
   },
   create: function(req, res){
     var message = new MessageModel({ name: req.body.name, phone: req.body.phone, email: req.body.email });
-    console.log(message);
     message.save(function(err){
       if (!err){
-        console.log("something good happened");
-        // res.render("messages/index")
-        // document.getElementsByClassName("form").className = "hidden";
-        // document.getElementsByClassName("thanks").className = "shown";
+        res.render("messages/thanks")
       } else {
-        console.log("something bad happened");
-        // res.render("messages/index");
+        res.render("messages/index");
       }
-
     })
-    debugger;
   }
 }
 
--- index.js
+++ index.js
@@ -1,31 +1,21 @@
-// express dependency for our application
 var express = require('express')
-// loads mongoose dependency
 var mongoose = require('mongoose')
-// loads dependency for middleware for paramters
 var bodyParser = require('body-parser')
-// loads dependency that allows put and delete where not supported in html
-var methodOverride = require('method-override')
-// loads module containing all messages contrller actions. not defined yet...
+
 var messagesController = require("./controllers/messagesController")
-// connect mongoose interfaces to reminders mongo db
+
 mongoose.connect('mongodb://localhost/messages')
+
 var app = express()
-// sets view engine to handlebars
+
 app.set("view engine", "hbs")
-// allows for parameters in JSON and html
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}))
-// allows for put/delete request in html form
-app.use(methodOverride('_method'))
-// connects assets like stylesheets
 app.use(express.static(__dirname + '/public'))
 
-// app server located on port 4000
 app.listen(4000, function(){
   console.log("app listening on port 4000")
 })
 
-// first route we'll define together ...
 app.get("/messages", messagesController.index);
 app.post("/messages", messagesController.create);
